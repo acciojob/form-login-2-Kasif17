8c535addd55234078c8cb6d017b87a65a63293f1
Mohd Kasif Khan <developer786kasif@gmail.com>
2026-05-08 00:27:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-//your JS code here. If required.
 function submitted() {
 	const fname = document.getElementById("fname").value.trim();
 	const lname = document.getElementById("lname").value.trim();
 	const number = document.getElementById("number").value.trim();
 	const email = document.getElementById("email").value.trim();
 
-	alert(`First Name: ${fname} Last Name: ${lname} Phone Number: ${number} Email ID: ${email}`);
+	alert(
+		`First Name: ${fname} 
+Last Name: ${lname} 
+Phone Number: ${number} 
+Email ID: ${email}`
+	);
 }
-
